fac20014234b06feec30e3bd40ab6aabd9253c17
Matthias Rudin <matthias.rudin@live.com>
2019-01-15 06:37:26 +0100

    Added Focus and OtherInput

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Sets the focus on input at page load
+$('input#name').focus();
+
+// If JavaScript is active, the other-title input will be hidden
+// If JavaScript is not active, the input will be shown
+$('input#other-title').hide();
