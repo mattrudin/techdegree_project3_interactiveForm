07ef490ea40025fed3e5bdf1dea5b8dcb2ba742e
Matthias Rudin <matthias.rudin@live.com>
2019-01-15 06:51:36 +0100

    Added eventhandler to jobTitle

--- js/script.js
+++ js/script.js
@@ -2,5 +2,13 @@
 $('input#name').focus();
 
 // If JavaScript is active, the other-title input will be hidden
-// If JavaScript is not active, the input will be shown
-$('input#other-title').hide();
+// If JavaScript is not active, the input will be shown -> obtrusive javscript principle
+const inputOtherTitle = $('input#other-title');
+inputOtherTitle.hide();
+
+// Eventhandlet for the job role selection:
+// If other is selected, the user shall see the hidden "other title" field
+$('select#title').on('change', (event) => {
+    let jobRoleInput = $(event.target).val();
+    jobRoleInput === 'other' ? inputOtherTitle.show() : inputOtherTitle.hide();
+});
